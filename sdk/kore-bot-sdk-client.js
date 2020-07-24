cddebfc48921b8f2ada1928b5fac42bfc2c1ed2b
RahulSiri <54171473+RahulSiri@users.noreply.github.com>
2020-07-24 08:43:59 +0100

    Update kore-bot-sdk-client.js
    changed the google.apis to https

--- sdk/kore-bot-sdk-client.js
+++ sdk/kore-bot-sdk-client.js
@@ -75,7 +75,7 @@ KoreBot.prototype.fetchUserLocation = function() {
 		var request = new XMLHttpRequest();
 
 	   var method = 'GET';
-	   var url = 'http://maps.googleapis.com/maps/api/geocode/json?latlng='+latitude+','+longitude+'&sensor=true';
+	   var url = 'https://maps.googleapis.com/maps/api/geocode/json?latlng='+latitude+','+longitude+'&sensor=true';
 	   var async = true;
 
 	   request.open(method, url, async);
